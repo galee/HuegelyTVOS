31b009fce1f546f9015b335f51f67473e2656218
Edward Gale <edward.gale@gmail.com>
2015-12-05 19:49:57 +0000

    Initial load

---
+++ TestFinal/HuegelyScene.swift
@@ -0,0 +1,33 @@
+//
+//  HuegelyScene.swift
+//  TestFinal
+//
+//  Created by Edward Gale on 05/12/2015.
+//  Copyright © 2015 Edward Gale. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class HuegelyScene {
+
+    var image: UIImage
+//    var name
+//    var lastModified
+    var lights:[HuueyLight]!
+
+    
+    public init() {
+        image = UIImage()
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+
+
+}
